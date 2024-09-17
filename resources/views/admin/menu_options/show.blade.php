b40412b696238faa5ed3915451f46dac65d35db3
Some558 <1641043@gmail.com>
2024-09-17 15:29:47 +0900

    献立詳細画面に献立名と献立タイプ表示

--- resources/views/admin/menu_options/show.blade.php
+++ resources/views/admin/menu_options/show.blade.php
@@ -1,13 +1,11 @@
 <x-admin-layout>
-    @if (session('success'))
-        <div class="alert alert-success text-green-500">
-            {{ session('success') }}
-        </div>
-    @endif
-    <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-12 mx-auto">
-            <div class="flex flex-col text-center w-full mb-12">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">献立詳細</h1>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-col text-center w-full mb-20">
+                <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">{{$menu_option->dish_name}}</h1>
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                    {{ $menu_option->dish_type}}
+                </p>
             </div>
         </div>
     </section>
