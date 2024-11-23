908f00ac4e5b36cec1e8feab7fc56457a52c44eb
Some558 <1641043@gmail.com>
2024-11-23 22:34:54 +0900

    コメント編集

--- resources/views/user/menu.blade.php
+++ resources/views/user/menu.blade.php
@@ -134,6 +134,7 @@ use App\Models\MenuOptions;
         </div>
     </div>
 
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
